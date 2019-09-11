c7dbee9d7388da3fe44fcd7425f9a9c36c8dce89
yao80350 <yao80350@gmail.com>
2019-09-11 12:23:59 +0800

    17.1 the Login Endpoint

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/concerts/{id}', 'concertController@show');
 Route::post('/concerts/{id}/orders', 'concertOrdersController@store');
 
 Route::get('/orders/{confirmationNumber}', 'ordersController@show');
+
+Route::post('/login', 'Auth\LoginController@login');
