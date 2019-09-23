e206b3d399b256d48ac6cfcf69241b5dacfd05e8
yao80350 <yao80350@gmail.com>
2019-09-23 12:38:11 +0800

    20.5 update database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,12 +14,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $concert = factory(Concert::class)->states("published")->create();
+        $user = factory(User::class)->create([
+            'email' => 'user@somewhere.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        $concert = factory(Concert::class)->states("published")->create([
+            'user_id' => $user->id,
+            'title' => "The Red Chord",
+            'subtitle' => "with Animosity and Lethargy",
+            'venue' => "The Mosh Pit",
+            'venue_address' => "123 Example Lane",
+            'city' => "Laraville",
+            'state' => "ON",
+            'zip' => "17916",
+            'date' => Carbon::parse('2019-12-13 8:00pm'),
+            'ticket_price' => 3250,
+            'additional_information' => "This concert is 19+.",
+        ])->addTickets(10);
+
+        factory(App\Concert::class)->create([
+            'user_id' => $user->id,
+            'title' => "Slayer",
+            'subtitle' => "with Forbidden and Testament",
+            'venue' => "The Rock Pile",
+            'venue_address' => "55 Sample Blvd",
+            'city' => "Laraville",
+            'state' => "ON",
+            'zip' => "19276",
+            'date' => Carbon::parse('2019-10-05 7:00pm'),
+            'ticket_price' => 5500,
+            'additional_information' => null,
+        ])->addTickets(10);
+
         $order = factory(Order::class)->create([
 			'confirmation_number' => 'ORDERCONFIRMATION1234',
         	'card_last_four' => '2666'
-		]);
+        ]);
+        
 		$ticket = factory(Ticket::class, 3)->create([
 			'concert_id' => $concert->id,
 			'order_id' => $order->id,
